74ca3145c521c2f95351cb7c9225094f428a973d
cassia <cassiamelvin@gmail.com>
2025-04-16 22:35:08 -0400

    fixed useSearchParams for pnpm buid

--- src/app/banner-bottom/page.tsx
+++ src/app/banner-bottom/page.tsx
@@ -1,55 +1,55 @@
-"use client";
+// "use client";
 
-import { useEffect, useState } from "react";
-import FlightBannerBottom from "@/components/FlightBannerBottom";
-import FlightApproachDisplay from "@/components/FlightApproachDisplay";
-import { flights } from "@/data/flightData.json";
+// import { useEffect, useState } from "react";
+// import FlightBannerBottom from "@/components/FlightBannerBottom";
+// import FlightApproachDisplay from "@/components/FlightApproachDisplay";
+// import { flights } from "@/data/flightData.json";
 
-export default function BannerTop() {
-  const [currentFlightIndex, setCurrentFlightIndex] = useState(0);
+// export default function BannerTop() {
+//   const [currentFlightIndex, setCurrentFlightIndex] = useState(0);
 
 
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentFlightIndex((prev) => 
-        prev === flights.length - 1 ? 0 : prev + 1
-      );
-    }, 5000);
-    return () => clearInterval(interval);
-  }, []);
+//   useEffect(() => {
+//     const interval = setInterval(() => {
+//       setCurrentFlightIndex((prev) => 
+//         prev === flights.length - 1 ? 0 : prev + 1
+//       );
+//     }, 5000);
+//     return () => clearInterval(interval);
+//   }, []);
 
-  const currentFlight = flights[currentFlightIndex];
+//   const currentFlight = flights[currentFlightIndex];
 
-  return (
-    <div className="min-h-screen ">
-      <div className="mt-[30px] p-6">
-      <FlightApproachDisplay
-          flight={{
-            title: "Upcoming Landing",
-            number: currentFlight.flight_number,
-            flight_number: currentFlight.flight_number.substring(0, 3),
-            destination_city: currentFlight.origin_city,
-            runway: currentFlight.runway,
-            originCode: currentFlight.origin,
-            registration: currentFlight.registration,
-          }}
-        />
-        <FlightBannerBottom />
-      </div>
+//   return (
+//     <div className="min-h-screen ">
+//       <div className="mt-[30px] p-6">
+//       <FlightApproachDisplay
+//           flight={{
+//             title: "Upcoming Landing",
+//             number: currentFlight.flight_number,
+//             flight_number: currentFlight.flight_number.substring(0, 3),
+//             destination_city: currentFlight.origin_city,
+//             runway: currentFlight.runway,
+//             originCode: currentFlight.origin,
+//             registration: currentFlight.registration,
+//           }}
+//         />
+//         <FlightBannerBottom />
+//       </div>
 
-      <div className="fixed bottom-20 left-10 flex gap-2">
-        <button
-          onClick={() => {
-            localStorage.removeItem("auth0_access_token");
-            localStorage.removeItem("auth0_refresh_token");
-            window.location.href = "/auth/signin";
-          }}
-          className="px-6 py-3 bg-blue-800 text-white rounded-[5px] hover:bg-red-700"
-        >
-          Sign Out
-        </button>
-      </div>
-    </div>
-  );
-}
+//       <div className="fixed bottom-20 left-10 flex gap-2">
+//         <button
+//           onClick={() => {
+//             localStorage.removeItem("auth0_access_token");
+//             localStorage.removeItem("auth0_refresh_token");
+//             window.location.href = "/auth/signin";
+//           }}
+//           className="px-6 py-3 bg-blue-800 text-white rounded-[5px] hover:bg-red-700"
+//         >
+//           Sign Out
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
